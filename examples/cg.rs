20033d64521c03f8a361d2ca859377963dc53839
Jim Turner <jturner314@gmail.com>
2017-10-05 15:02:10 -0400

    Add CG example

---
+++ examples/cg.rs
@@ -0,0 +1,25 @@
+extern crate rustimization;
+
+use rustimization::cg_minimizer::CG;
+
+const TRUE_MIN: [f64; 2] = [-1., 2.];
+
+fn f(x: &[f64]) -> f64 {
+    (x[0] - TRUE_MIN[0]).powi(4) + (x[1] - TRUE_MIN[1]).powi(4)
+}
+
+fn g(x: &[f64]) -> Vec<f64> {
+    let mut v = Vec::with_capacity(2);
+    v.push(4. * (x[0] - TRUE_MIN[0]).powi(3));
+    v.push(4. * (x[1] - TRUE_MIN[1]).powi(3));
+    v
+}
+
+fn main() {
+    let mut x = [0.; 2];
+    CG::new(&mut x, |x| (f(x), g(x)))
+        .minimize()
+        .expect("Failed to minimize");
+    println!("The minimum found is at {:?}", x);
+    println!("This should be close to {:?}", TRUE_MIN);
+}
